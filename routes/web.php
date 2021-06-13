3b32a3fd1b028cb6de89f9422efd66680d0cc758
DevafillaRS <devafillar.s@gmail.com>
2021-06-13 19:02:44 +0700

    add project Laravel CRUD

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'UsersController@login');
+
+Route::get('/login', 'UsersController@index'); 
+Route::post('/login', 'UsersController@login'); 
+
+Route::get('/dashboard', 'PagesController@index');
+
+Route::get('/all-article', 'ArticlesController@index');
+Route::get('/create-article', 'ArticlesController@create');
+Route::post('/create-article', 'ArticlesController@store');
+Route::get('/my-article', 'ArticlesController@my_article');
+Route::get('edit-article/{article}', 'ArticlesController@edit');
+Route::post('edit-article/{article}', 'ArticlesController@update');
+Route::get('delete/{id}', 'ArticlesController@destroy');
